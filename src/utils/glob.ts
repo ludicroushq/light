f66714e7d9e33850d2d465d57cc7d563b26ad940
Manthan Mallikarjun <nahtnam@gmail.com>
2019-04-18 09:02:27 -0700

    lint glob

--- src/utils/glob.ts
+++ src/utils/glob.ts
@@ -1,17 +1,16 @@
+/* eslint-disable max-len */
 import fs from 'fs';
 import path from 'path';
 import micromatch from 'micromatch';
 
-const ls: (dir: string, filelist: string[]) => any = (dir, filelist = []): string[] => fs.readdirSync(dir)
-  .map(file => (fs.statSync(path.join(dir, file)).isDirectory()
+const ls: (dir: string, filelist: string[]) => any = (dir, filelist = []): any[] => fs.readdirSync(dir)
+  .map((file: string): any[] => (fs.statSync(path.join(dir, file)).isDirectory()
     ? ls(path.join(dir, file), filelist)
     : filelist.concat(path.join(dir, file))[0]));
 
-const flatten = (arr: any[]): any[] => {
-  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
-}
+const flatten = (arr: any[]): any[] => arr.reduce((acc: any, val: any): any[] => (Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val)), []);
 
-export default (path: string, glob: string) => {
-  const files = flatten(ls(path, []));
+export default (url: string, glob: string): string[] => {
+  const files = flatten(ls(url, []));
   return micromatch(files, glob);
 };
