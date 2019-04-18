c01b03e99ee775e88ec266c6b9e16e9bea16ca2e
Manthan Mallikarjun <nahtnam@gmail.com>
2019-04-18 01:50:35 -0700

    normalize async functions in route

--- src/route.ts
+++ src/route.ts
@@ -1,3 +1,4 @@
+import { send } from 'micro';
 import { IncomingMessage, ServerResponse } from 'http';
 
 interface Route {
@@ -6,7 +7,16 @@ interface Route {
 }
 
 export default (route: Route): (req: IncomingMessage, res: ServerResponse) => {} => {
-  const fn = (req: IncomingMessage, res: ServerResponse): any => route.handler(req, res);
+  const fn = (req: IncomingMessage, res: ServerResponse): any => {
+    const run = async (): Promise<any> => route.handler(req, res);
+    return run().then((ret: any): any => {
+      if (ret) {
+        send(res, 200, ret);
+      }
+    }).catch((err): any => {
+      throw new Error(err);
+    });
+  };
   fn.path = route.path;
   return fn;
 };
