a0eddcfddef2b38778e010c723b66b8f28814eb9
Manthan Mallikarjun <nahtnam@gmail.com>
2019-04-18 19:39:24 -0700

    make badges links

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -17,11 +17,17 @@ export default () => (
       <div class="hero-foot has-text-centered">
         <div className="container">
           <div>
-            <img alt="CircleCI" src="https://img.shields.io/circleci/project/github/ludicrousxyz/light.svg?label=ci%20status&style=for-the-badge"></img>
+            <a href="https://circleci.com/gh/ludicrousxyz/light" target="_blank" rel="noopener noreferrer">
+              <img alt="CircleCI" src="https://img.shields.io/circleci/project/github/ludicrousxyz/light.svg?label=ci%20status&style=for-the-badge" />
+            </a>
             &nbsp;&nbsp;
-            <img alt="NPM" src="https://img.shields.io/npm/v/light.svg?label=npm%20version&style=for-the-badge"></img>
+            <a href="https://www.npmjs.com/package/light" target="_blank" rel="noopener noreferrer">
+              <img alt="NPM" src="https://img.shields.io/npm/v/light.svg?label=npm%20version&style=for-the-badge" />
+            </a>
             &nbsp;&nbsp;
-            <img alt="Coveralls" src="https://img.shields.io/coveralls/github/ludicrousxyz/light.svg?label=code%20coverage&style=for-the-badge"></img>
+            <a href="https://coveralls.io/github/ludicrousxyz/light" target="_blank" rel="noopener noreferrer">
+              <img alt="Coveralls" src="https://img.shields.io/coveralls/github/ludicrousxyz/light.svg?label=code%20coverage&style=for-the-badge" />
+            </a>
           </div>
         </div>
       </div>
