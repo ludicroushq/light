629695148131b232e5331bddfb968ef42d83a6ee
Manthan Mallikarjun <nahtnam@gmail.com>
2019-05-02 00:47:30 -0700

    fix(serverless): improve serverless experience

--- src/route.ts
+++ src/route.ts
@@ -16,45 +16,40 @@ type SR = ServerResponse;
 type AP = Promise<any>;
 
 export default (route: Route): Handler => {
-  const fn = (Req: IM, Res: SR): AP => run(Req, Res, async (req: IM, res: SR): AP => {
-    const middleware: any[] = route.middleware || [];
+  const fn = (Req: IM, Res: SR): AP => {
+    const exec = async (req: IM, res: SR): AP => {
+      const middleware: any[] = route.middleware || [];
 
-    if (fn.log !== false) {
-      const pinoOptions = process.env.NODE_ENV === 'production' ? {} : {
-        prettyPrint: {
-          levelFirst: true,
-        },
-      };
-      const logger = pino(pinoOptions);
-      middleware.unshift(logger);
+      if (fn.log !== false) {
+        const pinoOptions = process.env.NODE_ENV === 'production' ? {} : {
+          prettyPrint: {
+            levelFirst: true,
+          },
+        };
+        const logger = pino(pinoOptions);
+        middleware.unshift(logger);
+      }
+
+      for (const mw of middleware) { // eslint-disable-line
+        await mw(req, res); // eslint-disable-line
+      }
+
+      return route.handler(req, res);
+    };
+
+    const isAWS: boolean = !!(
+      (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV) || false
+    );
+    if (isAWS) {
+      return AWSServerlessMicro(exec)
     }
-
-    for (const mw of middleware) { // eslint-disable-line
-      await mw(req, res); // eslint-disable-line
-    }
-
-    return route.handler(req, res);
-  });
+    return run(Req, Res, exec);
+  }
 
   fn.path = route.path;
   fn.log = true;
   fn.module = __dirname;
-
-  const isAWS: boolean = !!(
-    (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV) || false
-  );
-  if (isAWS) {
-    return {
-      handler: AWSServerlessMicro(fn),
-    };
-  }
-
-  const isGCP: boolean = !!(process.env.X_GOOGLE_FUNCTION_NAME || false);
-  if (isGCP) {
-    return {
-      handler: fn,
-    };
-  }
+  fn.handler = fn;
 
   return fn;
 };
