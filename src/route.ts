29bbb99b61c4382750837dd46bb528befcf9916f
Manthan Mallikarjun <nahtnam@gmail.com>
2019-05-05 15:17:19 -0700

    fix(errors): add a proper error handler

--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,10 @@
-import { run } from 'micro';
+import { run, send } from 'micro';
 import { IncomingMessage, ServerResponse } from 'http';
 import AWSServerlessMicro from 'aws-serverless-micro';
 import pino from 'pino-http';
+
+const { NODE_ENV } = process.env;
+const DEV = NODE_ENV === 'development';
 
 interface Route {
   path?: string;
@@ -38,13 +41,28 @@ export default (route: Route): Handler => {
       return route.handler(req, res);
     };
 
+    const handleErrors = (hndlr: any): any => async (req: IM, res: SR): AP => {
+      try {
+        return await hndlr(req, res);
+      } catch (errorObj) {
+        const statusCode = errorObj.statusCode || errorObj.status || 500;
+        const message = statusCode ? errorObj.message : 'Internal Server Error';
+        if (errorObj instanceof Error) {
+          console.error(errorObj.stack); // eslint-disable-line
+        } else {
+          console.warn('thrown error must be an instance Error'); // eslint-disable-line
+        }
+        return send(res, statusCode, DEV ? errorObj.stack : { statusCode, message });
+      }
+    };
+
     const isAWS: boolean = !!(
       (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV) || false
     );
     if (isAWS) {
-      return AWSServerlessMicro(exec);
+      return AWSServerlessMicro(handleErrors(exec));
     }
-    return run(Req, Res, exec);
+    return run(Req, Res, handleErrors(exec));
   };
 
   Object.keys(route).forEach((key): void => {
