bc3a2abf6913db566b763d822237fe3f123997d9
Manthan Mallikarjun <nahtnam@gmail.com>
2019-08-13 00:00:16 -0700

    debug

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,7 @@
 import logger from './pino';
 
 export default (message: any): void => {
+  console.log('logger here with message', message)
   logger.logger.info(message)
+  console.log('done', logger)
 };
