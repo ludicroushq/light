a01ea3fb5dafadff08aa56cfe6b548904be0839e
Manthan Mallikarjun <nahtnam@gmail.com>
2019-08-20 10:57:07 -0700

    debug

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,5 @@
 import logger from './pino';
 
 export default (message: any): void => {
-  console.log('logger here with message', message);
-  logger.info(message);
-  console.log('done', logger.info);
-  logger.fatal('ADSD');
+  logger(message);
 };
