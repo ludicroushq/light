64df699eed5c9725177df0b8344ccb246fe20798
Manthan Mallikarjun <nahtnam@gmail.com>
2019-08-30 01:47:57 -0700

    add 404 handler

---
+++ tests/404.ts
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import join from 'url-join';
+
+import {
+  test, light, Route,
+} from '../src/index';
+
+let server: any;
+
+beforeEach(async () => {
+  server = await test(light(class Index extends Route {
+    public async handler() {
+      return {
+        hello: 'world',
+      };
+    }
+  }));
+});
+
+afterEach(async () => {
+  server.close();
+});
+
+describe('404', () => {
+  describe('with correct route', () => {
+    it('returns data', async () => {
+      expect.assertions(2);
+      const req = await fetch(server.url);
+      const res = await req.json();
+      expect(req.status).toStrictEqual(200);
+      expect(res).toMatchObject({ hello: 'world' });
+    });
+  });
+
+  describe('with incorrect route', () => {
+    it('returns a 404 error', async () => {
+      expect.assertions(2);
+      const req = await fetch(join(server.url, 'hello'));
+      const res = await req.text();
+      expect(req.status).toStrictEqual(404);
+      expect(res).toBe('Not Found');
+    });
+  });
+});
