d2879a2b309660b8d33621b832ec28b3af81cf02
Manthan Mallikarjun <nahtnam@gmail.com>
2020-01-20 18:43:08 -0800

    update docs

--- website/src/next.config.js
+++ website/src/next.config.js
@@ -1,38 +1,3 @@
 const withCSS = require("@zeit/next-css");
-const join = require('url-join');
-const guides = require('../../guides/guides.json');
-const docs = require('../../docs/docs.json');
 
-const { NODE_ENV, COMMIT_REF } = process.env;
-
-const mappedURL = `https://raw.githubusercontent.com/ludicrousxyz/light/${COMMIT_REF || 'master'}`;
-
-module.exports = withCSS({
-  async exportPathMap() {
-    const routes = {
-      '/': { page: '/' },
-    };
-
-    const processRoutes = (obj, prefix) => {
-      Object.keys(obj).forEach((key) => {
-        const guide = obj[key];
-        if (typeof guide !== 'string') {
-          return processRoutes(guide, prefix);
-        }
-        routes[join(prefix, guide)] = {
-          page: prefix,
-          query: { title: guide },
-        };
-        return null;
-      });
-    };
-
-    processRoutes(guides, '/guides');
-    processRoutes(docs, '/docs');
-
-    return routes;
-  },
-  env: {
-    BASE_URL: NODE_ENV === 'development' ? 'http://localhost:8080' : mappedURL,
-  },
-});
+module.exports = withCSS();
