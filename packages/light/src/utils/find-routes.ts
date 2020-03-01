590c214b64eb14e5cf08c8cb8f38909fab6170ac
Manthan Mallikarjun <nahtnam@gmail.com>
2020-03-01 15:33:20 -0800

    ignore the same files as jest

--- packages/light/src/utils/find-routes.ts
+++ packages/light/src/utils/find-routes.ts
@@ -6,7 +6,10 @@ const globAsync = promisify(glob);
 
 export default async (rootPath: string): Promise<string[]> => {
   const routesDir = join(rootPath, './routes');
-  const routes = await globAsync('**/*.js', { cwd: routesDir });
+  const routes = await globAsync('**/*.js', {
+    cwd: routesDir,
+    ignore: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  });
 
   return routes;
 };
