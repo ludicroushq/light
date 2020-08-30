d49a75d60017acc85f5155516a85223c09a14654
Manthan Mallikarjun <nahtnam@gmail.com>
2020-08-30 13:53:53 -0700

    add fastify to the benchmark

--- benchmark/dangerfile.js
+++ benchmark/dangerfile.js
@@ -3,6 +3,7 @@ const listen = require('test-listen');
 const fetch = require('node-fetch');
 const http = require('http');
 const { createServer } = require('light');
+const fastify = require('./fastify');
 
 const benchmark = async (url) => {
   for (let i = 0; i < 1000; i++) {
@@ -40,6 +41,20 @@ const benchmarkKoa = async () => {
   return end - start;
 };
 
+const benchmarkFastify = async () => {
+  const fastify = require('./fastify');
+  await fastify.ready();
+  const server = fastify.server;
+  const url = await listen(server);
+
+  const start = Date.now();
+  await benchmark(url);
+  const end = Date.now();
+
+  server.close();
+  return end - start;
+};
+
 const benchmarkLight = async () => {
   const server = createServer({ youch: false, requestLogger: false });
   const url = await listen(server.server);
@@ -58,6 +73,8 @@ const run = async () => {
   const express = await benchmarkExpress();
   console.log('Benchmarking Koa');
   const koa = await benchmarkKoa();
+  console.log('Benchmarking Fastify');
+  const fastify = await benchmarkFastify();
   console.log('Benchmarking light');
   const light = await benchmarkLight();
   const text = `
@@ -67,10 +84,12 @@ const run = async () => {
 
   Koa took **${koa}ms** to respond to 1k requests
 
+  Fastify took **${fastify}ms** to respond to 1k requests
+
   Light took **${light}ms** to respond to 1k requests
   `;
 
-  if (Math.min(express, koa, light) === light) {
+  if (Math.min(express, koa, fastify, light) === light) {
     message(text);
   } else {
     warn(text);
