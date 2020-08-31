8b4e55012b2b16e0c6dbaecfddf9f40573e88e42
Manthan Mallikarjun <nahtnam@gmail.com>
2020-08-30 22:42:12 -0700

    remove export

--- packages/light/src/index.ts
+++ packages/light/src/index.ts
@@ -4,5 +4,4 @@ export { createTest } from './create-test';
 export { createConfig } from './create-config';
 export { createLogger } from './create-logger';
 export { withConnect } from './with-connect';
-export { withServerless } from './with-serverless';
 export { logger } from './logger';
