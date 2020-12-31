a36dbd54744dc520ef4f34799858e2cea51aeb92
Manthan Mallikarjun <nahtnam@gmail.com>
2020-12-30 23:48:20 -0800

    fix test

--- packages/@lightjs/core/src/create-route/__tests__/methods.test.ts
+++ packages/@lightjs/core/src/create-route/__tests__/methods.test.ts
@@ -4,48 +4,7 @@ import { createRoute } from '../index';
 
 it('exports methods', () => {
   const useRoute = createRoute();
-  expect(useRoute).toMatchInlineSnapshot(`
-    Object {
-      "ACL": [Function],
-      "BIND": [Function],
-      "CHECKOUT": [Function],
-      "CONNECT": [Function],
-      "COPY": [Function],
-      "DELETE": [Function],
-      "GET": [Function],
-      "HEAD": [Function],
-      "LINK": [Function],
-      "LOCK": [Function],
-      "M-SEARCH": [Function],
-      "MERGE": [Function],
-      "MKACTIVITY": [Function],
-      "MKCALENDAR": [Function],
-      "MKCOL": [Function],
-      "MOVE": [Function],
-      "NOTIFY": [Function],
-      "OPTIONS": [Function],
-      "PATCH": [Function],
-      "POST": [Function],
-      "PROPFIND": [Function],
-      "PROPPATCH": [Function],
-      "PURGE": [Function],
-      "PUT": [Function],
-      "REBIND": [Function],
-      "REPORT": [Function],
-      "SEARCH": [Function],
-      "SOURCE": [Function],
-      "SUBSCRIBE": [Function],
-      "TRACE": [Function],
-      "UNBIND": [Function],
-      "UNLINK": [Function],
-      "UNLOCK": [Function],
-      "UNSUBSCRIBE": [Function],
-      "route": Object {
-        "middleware": Array [],
-      },
-      "useMiddleware": [Function],
-    }
-  `);
+  expect(Object.keys(useRoute).sort()).toEqual([...METHODS, 'route', 'useMiddleware'].sort());
 });
 
 it.each(METHODS)('supports http methods', (method) => {
