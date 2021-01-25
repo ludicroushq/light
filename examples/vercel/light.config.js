e37f857aa9b4f8c34c4e680f1b686303c329ab63
Manthan Mallikarjun <nahtnam@gmail.com>
2021-01-25 00:24:37 -0800

    fix example tests

--- examples/vercel/light.config.js
+++ examples/vercel/light.config.js
@@ -1,13 +1,20 @@
-const { useConfig, useLogger } = require('light');
+const { createConfig } = require('light');
 const winston = require('winston');
+
+const { config, useLogger } = createConfig();
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
 });
 
 useLogger({
   logger: () => logger,
 });
 
-module.exports = useConfig({});
+module.exports = config;
