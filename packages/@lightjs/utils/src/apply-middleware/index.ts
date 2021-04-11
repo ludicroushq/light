4e32ae6280ca35d0e3b675300f896d49ca56515a
Manthan Mallikarjun <nahtnam@gmail.com>
2021-04-11 01:32:15 -0700

    fix bug with middleware

--- packages/@lightjs/utils/src/apply-middleware/index.ts
+++ packages/@lightjs/utils/src/apply-middleware/index.ts
@@ -3,4 +3,5 @@ import { HandlerFunction, Middleware } from '@lightjs/types';
 export const applyMiddleware = (
   middleware: Middleware[],
   handler: HandlerFunction,
-): HandlerFunction => middleware.reverse().reduce((acc: any, val: any): any => val(acc), handler);
+): HandlerFunction =>
+  [...middleware].reverse().reduce((acc: any, val: any): any => val(acc), handler);
