bb77042c2ba1a0b0a3d8028568dacbbfe17e195c
Manthan Mallikarjun <nahtnam@gmail.com>
2022-01-17 17:24:12 -0800

    change how its optional

--- packages/@lightjs/test/src/createTest/index.ts
+++ packages/@lightjs/test/src/createTest/index.ts
@@ -1,7 +1,7 @@
 import { createServer } from '@lightjs/server';
 import { CreateTestOptions, CreateTest } from '@lightjs/types';
 
-export function createTest(opts?: CreateTestOptions): CreateTest {
-  const server = createServer({ middleware: opts?.middleware });
+export function createTest(opts: CreateTestOptions = {}): CreateTest {
+  const server = createServer({ middleware: opts.middleware });
   return server;
 }
