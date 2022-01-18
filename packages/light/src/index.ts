8dfdc602bba655b4c2d8a1d2040cfb40a82d5f96
Manthan Mallikarjun <nahtnam@gmail.com>
2022-01-17 17:12:16 -0800

    export middleware

--- packages/light/src/index.ts
+++ packages/light/src/index.ts
@@ -3,4 +3,12 @@ export { createTest, createRouteTest } from '@lightjs/test';
 export { createServer } from '@lightjs/server';
 export { createRoute, withConnect } from '@lightjs/core';
 export { useLogger } from '@lightjs/logger';
-export { Request, Response, Component, Logger, Context, HandlerFunction } from '@lightjs/types';
+export {
+  Request,
+  Response,
+  Component,
+  Logger,
+  Context,
+  HandlerFunction,
+  Middleware,
+} from '@lightjs/types';
