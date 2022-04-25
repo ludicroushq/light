9a31f18c848eec977e579898afcefbeaeb15eb17
Manthan Mallikarjun <nahtnam@gmail.com>
2022-04-25 01:43:50 -0700

    add light config import to server

--- packages/@lightjs/server/src/createServer/index.ts
+++ packages/@lightjs/server/src/createServer/index.ts
@@ -2,8 +2,12 @@ import micro from 'micro';
 import { IncomingMessage, ServerResponse } from 'http';
 import { CreateServerOptions, CreateServer } from '@lightjs/types';
 import { createRouter } from '@lightjs/router';
+import { importLightConfig } from '@lightjs/config';
 
 export function createServer({ middleware }: CreateServerOptions = {}): CreateServer {
+  // Import the light config so that if the user has any one-off scripts defined, they will execute
+  importLightConfig();
+
   const { router, reload, importedRoutes } = createRouter({ middleware });
 
   // create the http server
